d7cc1e6b886ad601598bf57f2b761229bd7e938e
209410769 <209410769@gms.tku.edu.tw>
2021-06-15 16:23:33 +0800

    portfolio

--- pages/portfolio/portfolio.js
+++ pages/portfolio/portfolio.js
@@ -3,22 +3,42 @@ export default function portfolio() {
     return (
         <div>
             <section className={styles.navigation}>
-                <div className={styles.nav-container}>
+                <div className={styles.container}>
                     <div className={styles.brand}>
                         <a href="#!">Logo</a>
                     </div>
                     <nav>
-                        <div className={styles.nav-mobile}><a id="nav-toggle" href="#"><span></span></a></div>
-                        <ul className={styles.nav-list}>
-                            <li>
-                                <a href="#">Home</a>
-                            </li>
-                            <li>
-                                <a href="#">About</a>
-                            </li>
-                            <li>
-                                <a href="#">Services</a>
-                                    <ul className={styles.nav-dropdown}>
+                        <div className={styles.mobile}><a   id={styles.toggle} href="#!">
+                            <span></span>
+                            </a>
+                        </div>
+                            <ul className={styles.list}>
+                                <li>
+                                    <a href="#!">Home</a>
+                                </li>
+                                <li>
+                                    <a href="#!">About</a>
+                                </li>
+                                <li>
+                                    <a href="#!">Services</a>
+                                 <ul className={styles.dropdown}>
+                                    <li>
+                                        <a href="#!">Web Design</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Web Development</a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">Graphic Design</a>
+                                    </li>
+                                </ul>
+                                </li>
+                                <li>
+                                    <a href="#!">Pricing</a>
+                                </li>
+                                <li>
+                                    <a href="#!">Portfolio</a>
+                                    <ul className={styles.dropdown}>
                                         <li>
                                             <a href="#!">Web Design</a>
                                         </li>
@@ -29,35 +49,20 @@ export default function portfolio() {
                                             <a href="#!">Graphic Design</a>
                                         </li>
                                     </ul>
-                            </li>
-                            <li>
-                                <a href="#!">Pricing</a>
-                            </li>
-                            <li>
-                                <a href="#!">Portfolio</a>
-                                    <ul className={styles.nav-dropdown}>
-                                        <li>
-                                            <a href="#!">Web Design</a>
-                                        </li>
-                                        <li>
-                                            <a href="#!">Web Development</a>
-                                        </li>
-                                        <li>
-                                            <a href="#!">Graphic Design</a>
-                                        </li>
-                                    </ul>
-                            </li>
-                            <li>
-                                <a href="#!">Contact</a>
-                            </li>
-                        </ul>
-                </nav>
-             </div>
-        </section>
-        <article>
-            <h2>Navigation</h2>
-            <p>This is my navigation bar.</p>
-        </article>
+                                </li>
+                                <li>
+                                    <a href="#!">Contact</a>
+                                </li>
+                            </ul>
+                    </nav>
+                </div>
+            </section>
+
+            <article>
+                <h2>Navigation</h2>
+                <p>Responsive Dropdown Navigation Bar.</p>
+                <p>Want to see how it's made? <a href="http://www.taniarascia.com/responsive-dropdown-navigation-bar/">Read the tutorial!</a></p>
+            </article>
         </div>
 
 
